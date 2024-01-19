4fb1fbaf554425777282efef46e0e40bdfe586c9
Imadbouchareb <153039617+Imadbouchareb@users.noreply.github.com>
2024-01-19 17:43:02 +0100

    Add files via upload

---
+++ Conditional Operator/src/operators/condOperator.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class condOperator {
+	public static void main(String[] args) {
+		boolean bool1, bool2, res;
+		bool1 = true;
+		bool2 = false;
+		
+		res = bool1 || bool2;
+		System.out.println("The result is: " +res);
+		
+		res = bool1 && bool2;
+		System.out.println("The result is: " +res);
+		
+	}
+
+}
